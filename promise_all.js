af736dc2f9af18f12b5a333837db375ae30c0594
Mayukh <mayukhchatterjee2023@gmail.com>
2023-03-20 19:09:49 +0530

    final

--- promise_all.js
+++ promise_all.js
@@ -1,58 +1,44 @@
-
-
-// Simulating an API request to create a post
-function createPost(post) {
+function updateUser(userId) {
     return new Promise((resolve) => {
         setTimeout(() => {
-            console.log(`Post "${post}" created at :${new Date().toLocaleString()}`);
+            console.log(` Last activity of user ${userId} at :  ${new Date().toLocaleString()}`);
             resolve();
-        }, 1000);
-    });
-}
-
-// Simulating an API request to update user's last activity time
-function updateLastUserActivityTime(userId) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log(`User ${userId} last activity updated`);
-            resolve();
-        }, 1000);
-    });
-}
-
-// Simulating an API request to delete a post
-function deletePost(postId) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log(`Post ${postId} deleted`);
-            resolve();
-        }, 1000);
+        }, 3000);
     });
 }
 
 
-const userId = 'Mayukh'; // replace with actual user id
-let posts = ["Post 1", "Post 2", "Post 3"]; // replace with actual posts
-function updateLastUserActivityTimeWithPostCreation() {
-    const newPost = { title: "New Post", body: "This is a new post" };
+function deletePost(userId, postId) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(` ${postId} deleted by ${userId}`);
+            resolve();
+        }, 5000);
+    });
+}
 
-    Promise.all([createPost(newPost), updateLastUserActivityTime(userId)])
+const userId = "Mayukh";
+let posts = ["Post 1", "Post 2", "Post 3"];
+
+function UpdateAppByUserActivity() {
+    updateUser(userId)
         .then(() => {
-            // Log all posts and last activity time of the user
-            console.log(`User ${userId} posts: ${posts.join(", ")}`);
-            console.log(`User ${userId} last activity time: ${new Date().toLocaleString()}`);
+            console.log(`${userId} have posted ---> ${posts} at: ${new Date().toLocaleString()}`);
 
-            // Delete the last post
-            const lastPostId = posts.pop();
-            return deletePost(lastPostId);
+            const postId = posts.pop();
+            return deletePost(userId, postId);
         })
         .then(() => {
-            // Log the new set of posts of the user
-            console.log(`User ${userId} posts after deletion: ${posts.join(", ")}`);
+            console.log(`${userId} have remaining ${posts} at: ${new Date().toLocaleString()}`);
+
+            const postId = posts.pop();
+            return deletePost(userId, postId);
+        })
+        .then(()=>{
+            console.log(`${userId} have remaining ${posts} at: ${new Date().toLocaleString()}`);
         })
         .catch((error) => {
-            console.error(error);
-        });
+            console.log("something Wrong");
+        })
 }
-
-updateLastUserActivityTimeWithPostCreation();
+UpdateAppByUserActivity();
